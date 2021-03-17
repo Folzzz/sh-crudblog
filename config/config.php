3f725324c1eb07a2d9fdf5ffd6316f17821aabae
Olaniyan Foluso <olaniyanfoluso498@gmail.com>
2021-03-17 17:01:46 +0100

    switched to a remote database

--- config/config.php
+++ config/config.php
@@ -1,19 +1,19 @@
 <?php
     //local development
-    DEFINE('DB_HOST', 'localhost');
-    DEFINE('DB_USER', 'root');
-    DEFINE('DB_PASSWORD', 'freshpeter498');
-    DEFINE('DB_NAME', 'sidehustleblog');
+    // DEFINE('DB_HOST', 'localhost');
+    // DEFINE('DB_USER', 'root');
+    // DEFINE('DB_PASSWORD', 'freshpeter498');
+    // DEFINE('DB_NAME', 'sidehustleblog');
 
-    DEFINE('ROOT_URL', 'http://localhost/phpsandbox/sidehustle-tasks/level4-crudblog/');
+    // DEFINE('ROOT_URL', 'http://localhost/phpsandbox/sidehustle-tasks/level4-crudblog/');
 
     //REMOTE DB
-    // DEFINE('DB_HOST', '');
-    // DEFINE('DB_USER', '');
-    // DEFINE('DB_PASSWORD', '');
-    // DEFINE('DB_NAME', '');
+    DEFINE('DB_HOST', 'remotemysql.com');
+    DEFINE('DB_USER', 'EpmRN3W61d');
+    DEFINE('DB_PASSWORD', 'nhHU7FWEeG');
+    DEFINE('DB_NAME', 'EpmRN3W61d');
 
-    // DEFINE('ROOT_URL', '');
+    DEFINE('ROOT_URL', 'https://sh-bloggers.herokuapp.com/');
 
     //create connection to database
     $db_conn = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die('Couldn\'t connect to database '. mysqli_connect_error());
